8dc43e0e1f333926d71a1f1058ded97bd7d753d2
SUFIAN\Sufian Vaio <sufian.zaabalawi@stud.uni-due.de>
2017-01-10 20:08:30 +0100

    close/open database

--- stud (1)/src/main/java/Strategy/DatabaseStrategy.java
+++ stud (1)/src/main/java/Strategy/DatabaseStrategy.java
@@ -2,66 +2,57 @@ package Strategy;
 
 import Database.OpenJPA;
 import fpt.com.Product;
-import fpt.com.SerializableStrategy;
 import fpt.com.db.AbstractDatabaseStrategy;
 
-
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 
 /**
- * Created by Sufian Vaio on 07.01.2017.
+ * Created by Team 10
+ * controller element of View.ViewShop
  */
+
 public class DatabaseStrategy extends AbstractDatabaseStrategy {
     OpenJPA openJPA = OpenJPA.getInstance();
-    int counter =0;
-    private FileOutputStream fos;
-    private FileInputStream fis;
-
+    int counter = 0;
     ArrayList<Model.Product> products;
-
-
-
 
 
     @Override
     public Product readObject() {
-            if (counter < products.size()) return products.get(counter++);
+        if (counter < products.size()) return products.get(counter++);
         return null;
     }
 
 
-
     @Override
     public void writeObject(Product obj) throws IOException {
-        open();
-        if(obj.getId() == 0){
-            openJPA.insert(obj.getName(),obj.getPrice(),obj.getQuantity());
+
+        if (obj.getId() == 0) {
+            openJPA.insert(obj.getName(), obj.getPrice(), obj.getQuantity());
         }
-        openJPA.close();
+
     }
 
     @Override
     public void close() throws IOException {
-        if(fis!=null) fis.close();
-        if(fos!=null)  fos.close();
-        if(!openJPA.isClosed())openJPA.close();
+        openJPA.close();
     }
+
     @Override
     public void open() throws IOException {
-        if(openJPA.isClosed())
         openJPA.open();
     }
 
     @Override
     public void open(InputStream input, OutputStream output) throws IOException {
+        open();
         if (output != null) {
-            open();
-            this.fos = (FileOutputStream) output;
             products = new ArrayList<>();
         }
         if (input != null) {
-            this.fis = (FileInputStream) input;
             products = openJPA.readList();
         }
     }
