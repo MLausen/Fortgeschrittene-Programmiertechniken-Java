f01f3b7cedc0ff4f1b6d6f73554fc1b60f5c98f6
Matti-PC\Matti <Schweiger-matthias@gmx.de>
2017-01-15 18:18:57 +0100

    Update + UDP requst and response

--- stud (1)/src/main/java/View/ViewCustomer.java
+++ stud (1)/src/main/java/View/ViewCustomer.java
@@ -8,6 +8,14 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
+import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Date;
+import java.util.Scanner;
+
 /**
  * Created by Team 10
  */
@@ -18,6 +26,9 @@ public class ViewCustomer extends BorderPane {
     Button b ;
     Button buy ;
     HBox buybox;
+
+    private Thread timeRequestThread;
+    private Thread timeResponseThread;
 
     public ViewCustomer(){
         buy = new Button("Buy");
@@ -43,6 +54,11 @@ public class ViewCustomer extends BorderPane {
         // positioning
         HBox box = new HBox(tableProducts,tableOrders);
         setCenter(box);
+
+        timeRequest();
+        timeRequestThread.start();
+        timeResponse();
+        timeResponseThread.start();
     }
 
     // method used in Controller to add Products from Model to the table in View
@@ -54,5 +70,129 @@ public class ViewCustomer extends BorderPane {
         TableColumn<Product,?> xColumn = new TableColumn<>(""+name);
         xColumn.setCellValueFactory(new PropertyValueFactory<>(""+name));
         return xColumn;
+    }
+
+    // method to send a request to a server by a client via udp-package
+    public void timeRequest(){
+        this.timeRequestThread = new Thread("Time Request") {
+            public void run() {
+                // own address
+                InetAddress ia = null;
+                try {
+                    ia = InetAddress.getByName("localhost");
+                } catch (UnknownHostException e2) {
+                    e2.printStackTrace();
+                }
+
+                // socket for client
+                try (DatagramSocket dSocket = new DatagramSocket(5555);) {
+
+                    try {
+                        int i = 0;
+                        while (i < 10) {
+                            String command = "TIME:";
+
+                            byte buffer[] = null;
+                            buffer = command.getBytes();
+
+                            // package for request
+                            DatagramPacket packet = new DatagramPacket(buffer,
+                                    buffer.length, ia, 6667);
+                            // sending package
+                            dSocket.send(packet);
+
+                            byte answer[] = new byte[1024];
+                            // preparing package for response
+                            packet = new DatagramPacket(answer, answer.length);
+                            // waiting for response
+                            dSocket.receive(packet);
+
+                            System.out.println(new String(packet.getData(), 0, packet
+                                    .getLength()));
+                            try {
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+
+                            i++;
+                        }
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+
+                } catch (SocketException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        };
+    }
+
+    // method to get a timeresponse by a server via udp-package
+    public void timeResponse(){
+        this.timeResponseThread = new Thread("Time Response") {
+            public void run() {
+                // server socket with port 6667
+                try (DatagramSocket socket = new DatagramSocket(6667);) {
+                    while (true) {
+
+                        // new package
+                        DatagramPacket packet = new DatagramPacket(new byte[5], 5);
+                        // waiting for package
+                        try {
+                            socket.receive(packet);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        // read data
+                        InetAddress address = packet.getAddress();
+                        int port = packet.getPort();
+                        int len = packet.getLength();
+                        byte[] data = packet.getData();
+
+                        System.out.printf(
+                                "Anfrage von %s vom Port %d mit der Länge %d:%n%s%n",
+                                address, port, len, new String(data, 0, len));
+
+                        // data to string
+                        String da = new String(packet.getData());
+                        // dividing datas by :
+                        try (Scanner sc = new Scanner(da).useDelimiter(":")) {
+                            // filtering first command
+                            String keyword = sc.next();
+
+                            if (keyword.equals("TIME")) {
+
+                                Date dt = new Date();
+                                byte[] myDate = dt.toString().getBytes();
+
+                                // package with new date
+                                packet = new DatagramPacket(myDate, myDate.length,
+                                        address, port);
+                                // sending package
+                                socket.send(packet);
+
+                            } else {
+                                byte[] myDate = null;
+                                myDate = new String("Command unknown").getBytes();
+
+                                // package for invalid keyword
+                                // preparing for response
+                                packet = new DatagramPacket(myDate, myDate.length,
+                                        address, port);
+                                // sending package
+                                socket.send(packet);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
     }
 }
