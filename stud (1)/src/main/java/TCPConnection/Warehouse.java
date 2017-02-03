c558ee2f12234bc83a515b2290b0a25d69fe6622
SUFIAN\Sufian Vaio <sufian.zaabalawi@stud.uni-due.de>
2017-02-03 15:49:23 +0100

    UDP just one Thread to all Requests

--- stud (1)/src/main/java/TCPConnection/Warehouse.java
+++ stud (1)/src/main/java/TCPConnection/Warehouse.java
@@ -8,6 +8,10 @@ import java.net.*;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by Team 10
@@ -57,16 +61,55 @@ public class Warehouse {
 
                         // new package
                         DatagramPacket packet = new DatagramPacket(new byte[5], 5);
-
                         // waiting for package
                         try {
                             socket.receive(packet);
-                            Thread timeClientThread = new Thread(new TimeClientThread(packet, socket));
-                            timeClientThread.start();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        // read data
+                        InetAddress address = packet.getAddress();
+                        int port = packet.getPort();
+
+                        // data to string
+                        String da = new String(packet.getData());
+
+                        // dividing commands by :
+                        try (Scanner sc = new Scanner(da).useDelimiter(":")) {
+                            // filtering first command
+                            String keyword = sc.next();
+
+                            if (keyword.equals("TIME")) {
+                                System.out.printf(
+                                        "Time-Request from %s  Port %d%n",
+                                        address, port);
+                                DateFormat df = new SimpleDateFormat("dd.MM.yy   HH:mm:ss");
+                                Date dateobj = new Date();
+                                byte[] myDate = df.format(dateobj).getBytes();
+
+                                // package with new date to the same Port and address
+                                packet = new DatagramPacket(myDate, myDate.length,
+                                        address, port);
+                                // sending package
+                                socket.send(packet);
+
+                            } else {
+                                byte[] myDate = null;
+                                myDate = new String("Command unknown").getBytes();
+
+                                // package for invalid keyword
+                                // preparing for response to the same Port and address
+                                packet = new DatagramPacket(myDate, myDate.length,
+                                        address, port);
+                                // sending package
+                                socket.send(packet);
+                            }
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -74,3 +117,18 @@ public class Warehouse {
         };
     }
 }
+/*
+    // method to get a timeresponse by a server via udp-package
+    public void timeResponse() {
+        // could happen?
+        if (this.timeResponseThread != null) {
+            return;
+        }
+
+        this.timeResponseThread = new Thread("Time Response") {
+            public void run() {
+                // server socket with port 6667
+                try (DatagramSocket socket = new DatagramSocket(6667)) {
+                    while (true) {
+
+                        */
